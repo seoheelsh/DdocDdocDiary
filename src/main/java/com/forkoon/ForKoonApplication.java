248359290d3fe2912b985cdd1edaddfef8cd1bb8
Seohee <seoheelsh@gmail.com>
2021-11-10 15:23:19 +0900

    New Project

---
+++ src/main/java/com/forkoon/ForKoonApplication.java
@@ -0,0 +1,13 @@
+package com.forkoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForKoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForKoonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/forkoon/ForKoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forkoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForKoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
