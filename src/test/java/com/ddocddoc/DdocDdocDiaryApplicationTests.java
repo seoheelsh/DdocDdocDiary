14b3da6e8a49876a075691106bb82cdac1619ac2
Seohee <seoheelsh@gmail.com>
2021-11-10 14:41:34 +0900

    프로젝트 생성

---
+++ src/test/java/com/ddocddoc/DdocDdocDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddocddoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdocDdocDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
